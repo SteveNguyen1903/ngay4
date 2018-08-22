94d112bc50dced936b7cb750afb786f1a7d1eb59
StevenNguyen <fall.legendary1992@gmail.com>
2018-08-22 11:54:28 +0700

    half progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,43 +8,44 @@
 // 		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
 // 	});
 // 	// Phần Clients
-// 	$('.home-clients .owl-carousel').owlCarousel({
-// 		items: 1,
-// 		nav: true,
-// 		dots: false,
-// 		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-// 		responsive: {
-// 			// breakpoint from 480 up
-// 			480: {
-// 				items: 2,
-// 			},
-// 			// breakpoint from 768 up
-// 			768: {
-// 				items: 4,
-// 			},
-// 			// breakpoint from 992 up
-// 			992: {
-// 				items: 6,
-// 			}
-// 		}
-// 	});
-// });
+	// $('.home-clients .owl-carousel').owlCarousel({
+	// 	items: 1,
+	// 	nav: true,
+	// 	dots: false,
+	// 	navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+	// 	responsive: {
+	// 		// breakpoint from 480 up
+	// 		480: {
+	// 			items: 2,
+	// 		},
+	// 		// breakpoint from 768 up
+	// 		768: {
+	// 			items: 4,
+	// 		},
+	// 		// breakpoint from 992 up
+	// 		992: {
+	// 			items: 6,
+	// 		}
+	// 	}
+	// });
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
 	nav:true,
-	dots:true,
+	dots:false,
 	navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
 	items:1,
     responsive:{
         480:{
-            items:2
+			items:3,
         },
         768:{
-            items:3
+			items:4,
         },
-        1024:{
-            items:6
+        992:{
+			items:6,
         }
     }
 })
