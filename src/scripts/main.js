5f8b3e9c9456ff42da1ed24de0d6fbda26e4c158
StevenNguyen <fall.legendary1992@gmail.com>
2018-08-22 11:10:43 +0700

    slider testing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,49 @@
-// Main
-$(document).ready(function () {
-	// Phần Slider
-	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-	});
-	// Phần Clients
-	$('.home-clients .owl-carousel').owlCarousel({
-		items: 1,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2,
-			},
-			// breakpoint from 768 up
-			768: {
-				items: 4,
-			},
-			// breakpoint from 992 up
-			992: {
-				items: 6,
-			}
-		}
-	});
-});
+// // Main
+// $(document).ready(function () {
+// 	// Phần Slider
+// 	$('.home-slider .owl-carousel').owlCarousel({
+// 		items: 1,
+// 		nav: false,
+// 		dots: true,
+// 		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+// 	});
+// 	// Phần Clients
+// 	$('.home-clients .owl-carousel').owlCarousel({
+// 		items: 1,
+// 		nav: true,
+// 		dots: false,
+// 		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+// 		responsive: {
+// 			// breakpoint from 480 up
+// 			480: {
+// 				items: 2,
+// 			},
+// 			// breakpoint from 768 up
+// 			768: {
+// 				items: 4,
+// 			},
+// 			// breakpoint from 992 up
+// 			992: {
+// 				items: 6,
+// 			}
+// 		}
+// 	});
+// });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	nav:true,
+	dots:false,
+	items:1,
+    responsive:{
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1024:{
+            items:6
+        }
+    }
+})
